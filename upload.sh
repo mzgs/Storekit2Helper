d57e6b027e3b09dff613caa1b90d50e3ba03b4a4
Mustafa <mzgsdev@gmail.com>
2024-04-05 21:38:26 +0300

    update to 0.0.3

---
+++ upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# File path to the pubspec.yaml
+FILE_PATH="pubspec.yaml"
+
+# Extract current version
+current_version=$(grep 'version: ' $FILE_PATH | sed 's/version: //')
+
+# Break down the version number into major, minor, and patch
+IFS='.' read -r -a version_parts <<< "$current_version"
+
+# Increment the patch version
+patch_version=$((version_parts[2] + 1))
+
+# Construct the new version number
+new_version="${version_parts[0]}.${version_parts[1]}.$patch_version"
+
+# Replace the old version with the new version in the file
+sed -i '' "s/version: $current_version/version: $new_version/" $FILE_PATH
+
+echo "Version updated to $new_version"
+
+flutter pub get
+
+dart pub publish -f
+
+git add .
+git commit -m "update to $new_version"
+git push
